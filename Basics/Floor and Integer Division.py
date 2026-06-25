9c2800cbfb9b8f6dd2b98334914e354f8f5efb6b
SuhaniCodes <Suhani190507@gmail.com>
2026-06-25 20:53:44 +0530

    Create Floor and Integer Division.py

---
+++ Basics/Floor and Integer Division.py
@@ -0,0 +1,11 @@
+Problem:Floor and Integer Division
+Concept:Division
+Platform:Hackerrank
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    integer_division = a//b
+    float_division = a/b
+    print(integer_division,float_division,sep="\n")
