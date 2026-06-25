83583488a70f4f6afe6660a8758919c00918d9e8
SuhaniCodes <Suhani190507@gmail.com>
2026-06-25 20:50:09 +0530

    Create Arithmetic Operators.py

---
+++ Basics/Arithmetic Operators.py
@@ -0,0 +1,12 @@
+Problem:Arithmetic Operators
+Concept:Add,Subtract,Multiply
+Platform:Hackerrank
+
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    sum = a+b
+    difference = a-b
+    product = a*b
+    print(sum,difference,product,sep="\n")
