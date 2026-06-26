21c02b6b35364d8e2b4d9d746899367df8dccb51
SuhaniCodes <Suhani190507@gmail.com>
2026-06-26 16:53:17 +0530

    Create Print Function.py

---
+++ Basics/Print Function.py
@@ -0,0 +1,9 @@
+Problem:Print Function
+Concept:Use of Print function in Loops
+Platform:Hackerrank
+
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,n+1):
+        print(i,end="")
