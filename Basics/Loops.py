dd7a33f44cd0e4918142819e8fa2653f48e462bc
Suhanicodes123 <Suhani190507@gmail.com>
2026-06-28 15:15:09 +0530

    Update Loops.py

--- Basics/Loops.py
+++ Basics/Loops.py
@@ -2,10 +2,10 @@ Problem:Loops
 Concept:For Loops
 Platform:Hackerrank
 
-
+#print integers 1 to n without spacing
     n = int(input())
     i=0
-    for i in range(n+1):
+    for i in range(1,n+1):
         print(i**2) 
         
         i+=1
