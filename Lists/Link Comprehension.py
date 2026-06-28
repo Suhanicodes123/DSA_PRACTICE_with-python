8e57c0078b335f479d795cbc87eb15b04f942846
SuhaniCodes <Suhani190507@gmail.com>
2026-06-28 17:11:27 +0530

    Create Link Comprehension.py

---
+++ Lists/Link Comprehension.py
@@ -0,0 +1,19 @@
+Problem:List Comprehension
+Concept:List Comprehension
+Platform:Hackerrank
+
+
+y = int(input())
+    z = int(input())
+    n = int(input())
+    result=[]
+    
+    for x in rax = int(input())
+    nge(0,x+1):
+        for y in range(0,y+1):
+            for z in range(0,z+1):
+                if x+y+z!=n:
+                    result.append([x,y,z])
+    print(result)
+                    
+    
