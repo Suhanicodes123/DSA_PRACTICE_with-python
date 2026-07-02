b6338ee5a332465d34d4e5f812bfde0ec2b1c04f
SuhaniCodes <Suhani190507@gmail.com>
2026-07-02 19:53:17 +0530

    Create find a string.py

---
+++ Strings/find a string.py
@@ -0,0 +1,12 @@
+Problem:Strings
+Concept:Find a String
+Platform:Hackerrank
+
+
+def count_substring(string, sub_string):
+    count=0
+    
+    for i in range(len(string)):
+        if string[i:].startswith(sub_string):
+            count+=1
+    return count
