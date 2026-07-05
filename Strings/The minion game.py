b51d1b37a5fd6c79a7a26931868b82b235086ab3
SuhaniCodes <Suhani190507@gmail.com>
2026-07-05 21:54:49 +0530

    Create The minion game.py

---
+++ Strings/The minion game.py
@@ -0,0 +1,23 @@
+Problem = Minion game
+Platform = Hackerrank
+Concept = Strings/loops
+
+
+def minion_game(string):
+    vowels = "AEIOU"
+    kevin = 0
+    stuart = 0
+    n = len(string)
+    
+    for i in range(n):
+        if string[i] in vowels:
+            kevin += n-i
+        else:
+            stuart += n-i
+            
+    if kevin > stuart:
+        print("Kevin", kevin)
+    elif stuart > kevin:
+        print("Stuart", stuart)
+    else:
+        print("Draw")
