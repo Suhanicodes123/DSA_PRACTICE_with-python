9caba4b810fea5cbaab43cc4db69713a7d20595d
SuhaniCodes <Suhani190507@gmail.com>
2026-07-11 21:20:36 +0530

    Create Triangle_quest.py

---
+++ Basics/Triangle_quest.py
@@ -0,0 +1,8 @@
+# Problem: Triangle Quest
+# Platform: HackerRank
+# Concept: Loops / Pattern Printing
+# Condition:  should be done in not more than 2 lines of code
+
+
+for i in range(1,int(input())+1):
+    print(i*((10**i-1)//9))
