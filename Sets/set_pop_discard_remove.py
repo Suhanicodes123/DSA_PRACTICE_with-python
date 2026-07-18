5a4e8c61b91da5449bab98a73c636f97e85fef4c
Suhanicodes123 <Suhani190507@gmail.com>
2026-07-18 22:42:57 +0530

    Update set_pop_discard_remove.py

--- Sets/set_pop_discard_remove.py
+++ Sets/set_pop_discard_remove.py
@@ -9,7 +9,6 @@ for _ in range(int(input())):
     command = input().split() # Read the command and its argument (if any)
 
     if command[0] == "pop":
-        
         s.pop()
     elif command[0] == "remove":
         s.remove(int(command[1]))  #gives error if element is not present
