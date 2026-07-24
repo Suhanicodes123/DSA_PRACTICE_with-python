4fb96c2ef38739b0a4a4a4f9d0e90125afa1ed8a
Suhanicodes123 <Suhani190507@gmail.com>
2026-07-24 23:18:29 +0530

    Update Link Comprehension.py

--- Lists/Link Comprehension.py
+++ Lists/Link Comprehension.py
@@ -3,17 +3,19 @@ Concept:List Comprehension
 Platform:Hackerrank
 
 
+x = int(input())
 y = int(input())
-    z = int(input())
-    n = int(input())
+z = int(input())
+n = int(input())
+   
     result=[]
     
-    for x in rax = int(input())
-    nge(0,x+1):
-        for y in range(0,y+1):
-            for z in range(0,z+1):
-                if x+y+z!=n:
-                    result.append([x,y,z])
+    for i in range(0,x+1):
+        for j in range(0,y+1):
+            for k in range(0,z+1):
+                if i+j+k!=n:
+                    result.append([i,j,k])
+    
     print(result)
                     
     
